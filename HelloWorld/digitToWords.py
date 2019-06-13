c51fe2e0b11a04914bf7ac0c86b1ee035da773f6
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 17:59:07 +0600

    digits to words

---
+++ HelloWorld/digitToWords.py
@@ -0,0 +1,16 @@
+digits = input("Phone: ")
+
+digits_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+}
+
+output = ''
+
+for ch in digits:
+    output += digits_mapping.get(ch, "!") + " "
+
+print(output)
