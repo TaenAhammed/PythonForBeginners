534480b468fd009a6fe0dabd3c9be73a57bd1c56
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 13:44:22 +0600

    nested loops.

---
+++ HelloWorld/nestedLoops.py
@@ -0,0 +1,15 @@
+# Generateing coordinates
+
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
+
+
+# Exercise
+numbers = [5, 2, 5, 2, 2]
+
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'X'
+    print(output)
