5757d6930d1fe2af233624f56bb06606a0b4a610
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 13:24:32 +0600

    for loops

---
+++ HelloWorld/forLoops.py
@@ -0,0 +1,9 @@
+# for item in 'Taen Ahammed':
+# for item in ["Taen", "Tarif", "Tamim"]:
+for item in [1, 2, 3, 4]:
+    print(item)
+
+# for i in range(10):
+# for i in range(10, 20):
+for i in range(10, 20, 2):
+    print(i)
