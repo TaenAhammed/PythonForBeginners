b7db769c2dde4f235f6aeba06a3d1cf1c7154b7b
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 13:33:50 +0600

    for loops exercise.

--- HelloWorld/forLoops.py
+++ HelloWorld/forLoops.py
@@ -7,3 +7,12 @@ for item in [1, 2, 3, 4]:
 # for i in range(10, 20):
 for i in range(10, 20, 2):
     print(i)
+
+# Exercise
+prices = [10, 20, 30, 40]
+total_price = 0
+
+for price in prices:
+    total_price += price
+
+print(f"Total: {total_price}")
