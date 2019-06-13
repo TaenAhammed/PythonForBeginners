3c62f5c8a892982ff812149b1fcb7633ff7069fe
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 06:47:55 +0600

    strings

---
+++ HelloWorld/strings.py
@@ -0,0 +1,27 @@
+course = 'Python\'s Course for Beginners'
+
+email = '''
+Hi Taen,
+Here is our first email to you.
+Thank you,
+The support team
+'''
+
+print(course)
+print(email)
+
+course = 'Python for Beginners'
+another = course[:]
+print(another)
+
+print(course[0])
+print(course[-2])
+print(course[0:3])
+print(course[0:])
+print(course[2:])
+print(course[:5])
+print(course[:])
+
+# Exercise
+name = 'Jennifer'
+print(name[1:-1])  # ennife
