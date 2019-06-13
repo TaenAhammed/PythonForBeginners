d063c7dff6e65f504027752899d0218210b74257
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 10:50:07 +0600

    comparison operator

---
+++ HelloWorld/comparisonOperator.py
@@ -0,0 +1,9 @@
+name = 'Taen Ahammed'
+name_len = len(name)
+
+if(name_len < 3):
+    print("name must be at least 3 characters.")
+elif(name_len > 50):
+    print("name can be a maximum of 50 characters.")
+else:
+    print("name looks good!")
