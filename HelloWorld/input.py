56bb4442f36e09a4fab55e49974c8f8c52094abb
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 06:17:07 +0600

    getting input

---
+++ HelloWorld/input.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+print("Hi " + name)
+
+# Exercise
+name = input('What\'s your name? ')
+fav_color = input('What\'s your favorite color? ')
+
+print(name + " likes " + fav_color)
