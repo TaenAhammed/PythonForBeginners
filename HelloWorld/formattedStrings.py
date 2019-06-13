e4660e8b06ca8424b67d1e0f8093eec6d1793e6d
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 06:55:19 +0600

    formatted strings

---
+++ HelloWorld/formattedStrings.py
@@ -0,0 +1,8 @@
+first_name = "Taen"
+last_name = "Ahammed"
+
+message = first_name + ' [' + last_name + '] is a coder'  # bad
+msg = f'{first_name} [{last_name}] is a coder'
+
+print(message)
+print(msg)
