40265be4f66d190407f65a1988ff688414862a54
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 11:02:32 +0600

    weightConverter

---
+++ HelloWorld/weightConverter.py
@@ -0,0 +1,11 @@
+weight = input('Weight: ')
+unit = input('(L)bs or (K)g: ')
+
+if (unit == 'L') or (unit == 'l'):
+    converted_weight = int(weight) * 0.45
+    print(f"Your are {converted_weight} kilos")
+elif (unit == 'K') or (unit == 'k'):
+    converted_weight = int(weight) / 0.45
+    print(f"Your are {converted_weight} pounds")
+else:
+    print("You've not entered any unit or entered wrong unit.")
