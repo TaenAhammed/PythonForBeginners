0eef418015fdc7a5878a32f971620883adccc364
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 06:31:29 +0600

    type conversion

---
+++ HelloWorld/typeConversion.py
@@ -0,0 +1,12 @@
+birth_year = input("Birth year: ")
+print(type(birth_year))
+
+age = 2019 - int(birth_year)
+print(type(age))
+
+print(age)
+
+# Exercise
+weight_lbs = input("Weight (lbs): ")
+weight_kgs = int(weight_lbs) * 0.45
+print(weight_kgs)
