c45c537d4d2874f211843266935ca8a777e0c066
Taen Ahammed <taenahammed7@gmail.com>
2019-06-13 06:01:34 +0600

    variables

---
+++ HelloWorld/variables.py
@@ -0,0 +1,12 @@
+price = 10
+price = 20
+rating = 4.9
+name = "Programming 101"
+is_published = True
+
+print(price)
+
+# Exercise
+patient_name = "John Smith"
+patient_age = 20
+is_new_patient = True
