0903b3993282828f80ceab2a0cdef60b6af213a0
Taen Ahammed <taenahammed7@gmail.com>
2019-06-14 16:45:59 +0600

    keyword arguments

--- HelloWorld/functions.py
+++ HelloWorld/functions.py
@@ -3,4 +3,8 @@ def greet_user(first_name, last_name):
     print("Welcome to the class")
 
 
-greet_user('Taen', 'Ahammed')
+# greet_user('Taen', 'Ahammed')
+
+# Keyword arguments
+greet_user(last_name='Taen', first_name='Ahammed')
+greet_user('Tarif', last_name='Ahammed')
