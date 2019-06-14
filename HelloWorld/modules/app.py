0be81a66cf0bbe37c789abcdc3296f9dddc6f9d0
Taen Ahammed <taenahammed7@gmail.com>
2019-06-14 22:07:41 +0600

    modules

---
+++ HelloWorld/modules/app.py
@@ -0,0 +1,9 @@
+# importing entire module
+import converter
+
+# importing specific function of class from a module
+from converter import lbs_to_kgs
+
+print(converter.lbs_to_kgs(160))
+
+print(lbs_to_kgs(160))
