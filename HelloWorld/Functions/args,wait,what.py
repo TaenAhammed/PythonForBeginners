908f825ee77541500322a96863ffddaca33fc767
Taen Ahammed <taenahammed7@gmail.com>
2019-06-24 21:52:57 +0600

    **args *args produces tuple and ** args produces dictionary.

--- HelloWorld/Functions/args,wait,what.py
+++ HelloWorld/Functions/args,wait,what.py
@@ -2,6 +2,9 @@
 
 
 def increment(*numbers):
+    # tuple
+    print(numbers)
+
     total = 0
     for number in numbers:
         total += number
@@ -9,3 +12,15 @@ def increment(*numbers):
 
 
 print(increment(1, 2, 3, 4))
+
+# **args
+
+
+def save_user(**user):
+    # dictionary
+    print(user)
+
+    print(user['name'])
+
+
+save_user(id=1, name='Taen Ahammed', age=20)
