5f93f3d9acc4d6f3379bf70cb66014e41a7c522d
Taen Ahammed <taenahammed7@gmail.com>
2019-06-25 11:05:36 +0600

    lists

---
+++ HelloWorld/Data Structures/lists2.py
@@ -0,0 +1,15 @@
+letters = ['a', 'b', 'c']
+matrix = [[0, 0], [1, 2]]
+
+zeros = [0] * 5
+print(zeros)
+
+combined = zeros + letters
+print(combined)
+
+numbers = list(range(20))
+print(numbers)
+
+chars = list("Hello World")
+print(chars)
+print(len(chars))
